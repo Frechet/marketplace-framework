d9b39307247a0092decd06766c527d8677ba5ae1
Victor Borovlev <borovl.victor@gmail.com>
2022-06-26 18:10:21 +0700

    fix keycloak start and try to config jwt

--- wb-retailer/src/main/kotlin/dev/marketplace/Application.kt
+++ wb-retailer/src/main/kotlin/dev/marketplace/Application.kt
@@ -1,17 +1,21 @@
 package dev.marketplace
 
+import com.typesafe.config.ConfigFactory
 import dev.marketplace.plugins.configureRouting
 import dev.marketplace.plugins.configureSecurity
 import dev.marketplace.plugins.configureSerialization
 import dev.marketplace.plugins.configureTemplating
+import io.ktor.server.config.*
 import io.ktor.server.engine.*
 import io.ktor.server.jetty.*
 
 fun main() {
-    embeddedServer(Jetty, port = 8080, host = "0.0.0.0") {
-        configureRouting()
-        configureSecurity()
-        configureTemplating()
-        configureSerialization()
-    }.start(wait = true)
+    embeddedServer(Jetty, environment = applicationEngineEnvironment {
+        config = HoconApplicationConfig(ConfigFactory.load())
+        connector {
+            port = 8081
+            host = "0.0.0.0"
+        }
+
+    }).start(wait = true)
 }
